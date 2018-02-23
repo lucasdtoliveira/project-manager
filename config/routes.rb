5edb8de4ae596f20f47df294bb6625c0836eff19
lucasdtoliveira <lucastavares@poli.ufrj.br>
2018-02-23 14:15:08 -0300

    Sign Up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+
   root 'static_pages#home'
 
   get 'static_pages/home'
@@ -11,5 +12,6 @@ Rails.application.routes.draw do
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'  
+  get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
