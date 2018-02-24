c9fd28d082b9220eaf71ad20fa4ba7a2805e53e8
lucasdtoliveira <lucastavares@poli.ufrj.br>
2018-02-24 15:31:22 -0300

    Basic Login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    
     if @user.save
+      log_in @user
       flash[:success] = "You've successfuly signed up for Project Manager"
       redirect_to @user
     else
